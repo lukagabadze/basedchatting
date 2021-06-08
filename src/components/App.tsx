f305b1721c3e60999c882bd7cca5f44bbc162201
lukagabadze <gabadzeluka2003@gmail.com>
2021-06-08 22:03:34 +0400

    custom theme

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,41 +5,52 @@ import { AuthProvider } from "../contexts/AuthContext";
 import { SocketProvider } from "../contexts/SocketContext";
 import Chat from "./Chat/Chat";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
 import PrivateRoute from "./PrivateRoute";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#ffc942",
+    },
+  },
+});
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <SocketProvider>
-          <Router>
-            <div
-              style={{
-                height: "100vh",
-                display: "flex",
-                flexDirection: "column",
-              }}
-            >
-              <Header />
+      <ThemeProvider theme={theme}>
+        <AuthProvider>
+          <SocketProvider>
+            <Router>
               <div
                 style={{
-                  flex: 1,
-                  overflowY: "auto",
+                  height: "100vh",
+                  display: "flex",
+                  flexDirection: "column",
                 }}
               >
-                <Switch>
-                  <Route path="/auth">
-                    <Auth />
-                  </Route>
-                  <PrivateRoute path="" component={Chat} />
-                </Switch>
+                <Header />
+                <div
+                  style={{
+                    flex: 1,
+                    overflowY: "auto",
+                  }}
+                >
+                  <Switch>
+                    <Route path="/auth">
+                      <Auth />
+                    </Route>
+                    <PrivateRoute path="" component={Chat} />
+                  </Switch>
+                </div>
               </div>
-            </div>
-          </Router>
-        </SocketProvider>
-      </AuthProvider>
+            </Router>
+          </SocketProvider>
+        </AuthProvider>
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }
