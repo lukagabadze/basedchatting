60d062b2e9ea512da009a6c0b6cadaa63f772b3a
lukagabadze <gabadzeluka2003@gmail.com>
2021-06-09 00:19:19 +0400

    moved providers to index

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,57 +1,35 @@
 import Header from "./Header/Header";
 import Auth from "./Auth/Auth";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { AuthProvider } from "../contexts/AuthContext";
-import { SocketProvider } from "../contexts/SocketContext";
 import Chat from "./Chat/Chat";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { createMuiTheme, ThemeProvider } from "@material-ui/core";
 import PrivateRoute from "./PrivateRoute";
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#ffc942",
-    },
-  },
-});
-
-const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <AuthProvider>
-          <SocketProvider>
-            <Router>
-              <div
-                style={{
-                  height: "100vh",
-                  display: "flex",
-                  flexDirection: "column",
-                }}
-              >
-                <Header />
-                <div
-                  style={{
-                    flex: 1,
-                    overflowY: "auto",
-                  }}
-                >
-                  <Switch>
-                    <Route path="/auth">
-                      <Auth />
-                    </Route>
-                    <PrivateRoute path="" component={Chat} />
-                  </Switch>
-                </div>
-              </div>
-            </Router>
-          </SocketProvider>
-        </AuthProvider>
-      </ThemeProvider>
-    </QueryClientProvider>
+    <Router>
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Header />
+        <div
+          style={{
+            flex: 1,
+            overflowY: "auto",
+          }}
+        >
+          <Switch>
+            <Route path="/auth">
+              <Auth />
+            </Route>
+            <PrivateRoute path="" component={Chat} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { AuthProvider } from "./contexts/AuthContext";
+import { SocketProvider } from "./contexts/SocketContext";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#ffc942",
+    },
+  },
+});
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <AuthProvider>
+          <SocketProvider>
+            <App />
+          </SocketProvider>
+        </AuthProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
