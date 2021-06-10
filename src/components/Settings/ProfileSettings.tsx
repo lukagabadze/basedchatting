b5f102dc503d6dc45203852f9cfa2cfb07dc70fd
lukagabadze <gabadzeluka2003@gmail.com>
2021-06-10 22:35:57 +0400

    ability to change profile image in profile settings

--- src/components/Settings/ProfileSettings.tsx
+++ src/components/Settings/ProfileSettings.tsx
@@ -1,30 +1,67 @@
 import React, { ReactElement, useState } from "react";
-import { Typography, TextField, Button } from "@material-ui/core";
-
+import { Typography, TextField, Button, makeStyles } from "@material-ui/core";
 import { useAuth } from "../../contexts/AuthContext";
-import { database } from "../../firebase";
+import { database, storage } from "../../firebase";
 
-interface Props {}
+const useStyles = makeStyles((theme) => ({
+  textInput: {
+    marginRight: theme.spacing(2),
+  },
+  fileInputDiv: {
+    marginTop: theme.spacing(3),
+  },
+  fileInput: {
+    display: "none",
+  },
+  imagePreview: {
+    maxWidth: "250px",
+    maxHeight: "500px",
+    objectFit: "contain",
+    marginTop: theme.spacing(1),
+  },
+  submitButton: {
+    marginTop: theme.spacing(3),
+    textTransform: "none",
+  },
+}));
 
-export default function ProfileSettings({}: Props): ReactElement {
+export default function ProfileSettings(): ReactElement {
   const { user } = useAuth();
+  const classes = useStyles();
+
   const [nameInput, setNameInput] = useState(user ? user.displayName : "");
+  const [image, setImage] = useState<File | null>(null);
+  const [imagePreview, setImagePreview] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
 
   if (!user) return <Typography variant="h1">You must be logged in</Typography>;
 
   const formValid: boolean =
-    nameInput && nameInput !== user.displayName ? true : false;
+    (nameInput && nameInput !== user.displayName) || image ? true : false;
 
-  function formSubmitHandler(e: React.FormEvent) {
+  async function formSubmitHandler(e: React.FormEvent) {
     e.preventDefault();
     if (!user) return;
-    if (!nameInput) return;
     if (!formValid) return;
 
-    database
-      .collection("users")
-      .doc(user.uid)
-      .update({ displayName: nameInput });
+    setLoading(true);
+
+    let updatedUser = {
+      displayName: nameInput,
+      imageUrl: user.imageUrl,
+    };
+
+    if (image) {
+      const fileName = `${user.uid}-${image.name}`;
+      const storageRef = storage.ref(`profile-images/${fileName}`);
+      await storageRef.put(image);
+      const url = await storageRef.getDownloadURL();
+      updatedUser.imageUrl = url;
+    }
+
+    await database.collection("users").doc(user.uid).update(updatedUser);
+
+    setLoading(false);
   }
 
   return (
@@ -35,7 +72,7 @@ export default function ProfileSettings({}: Props): ReactElement {
 
       {/* Display name */}
       <TextField
-        defaultValue={user.displayName}
+        className={classes.textInput}
         label="Display name"
         variant="outlined"
         fullWidth
@@ -46,15 +83,47 @@ export default function ProfileSettings({}: Props): ReactElement {
         }
       />
 
+      <div className={classes.fileInputDiv}>
+        <Typography variant="h5" display="inline">
+          Profile image:{" "}
+        </Typography>
+        <label htmlFor="profile-image-upload">
+          <Button variant="contained" color="primary" component="span">
+            Upload
+          </Button>
+        </label>
+        <input
+          className={classes.fileInput}
+          type="file"
+          accept="image/*"
+          id="profile-image-upload"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            const files = e.target.files;
+            if (!files) return;
+
+            setImage(files[0]);
+            setImagePreview(URL.createObjectURL(files[0]));
+          }}
+        />
+        {imagePreview && (
+          <>
+            <br />
+            <img className={classes.imagePreview} src={imagePreview} />
+          </>
+        )}
+      </div>
+
       {/* Submit button */}
       <Button
+        className={classes.submitButton}
+        disableElevation
         type="submit"
-        color="primary"
+        color="secondary"
         variant="contained"
         disabled={!formValid}
-        style={{ margin: "auto" }}
+        fullWidth
       >
-        Submit
+        {loading ? "Loading..." : "Save"}
       </Button>
     </form>
   );
