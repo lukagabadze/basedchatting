08d506fa79371dbcf2ddc12392984e94f0df15b2
lukagabadze <gabadzeluka2003@gmail.com>
2021-06-12 23:56:04 +0400

    avatarContext that maps all the contact members uids to their imageUrls and stores it in state

---
+++ src/contexts/AvatarContext.tsx
@@ -0,0 +1,73 @@
+import React, {
+  createContext,
+  ReactElement,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { database } from "../firebase";
+
+type AvatarContextType = {
+  userAvatarMap: userAvatarMapType;
+  fetchAndMapUsers(members: string[]): userAvatarMapType;
+};
+
+const defaultValue = {
+  userAvatarMap: {},
+  fetchAndMapUsers: () => {
+    return {};
+  },
+};
+
+const AvatarContext = createContext<AvatarContextType>(defaultValue);
+
+export function useAvatar(members: string[]) {
+  const { fetchAndMapUsers } = useContext(AvatarContext);
+  return fetchAndMapUsers(members);
+}
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export type userAvatarMapType = {
+  [key: string]: string;
+};
+
+export default function AvatarProvider({ children }: Props): ReactElement {
+  const [userAvatarMap, setUserAvatarMap] = useState<userAvatarMapType>({});
+  let avatarUrlMap: userAvatarMapType = { ...userAvatarMap };
+  let render = false;
+
+  useEffect(() => {
+    // Save mapped data to state
+    setUserAvatarMap(avatarUrlMap);
+  }, [render]);
+
+  // Fetch and map all the members profile images
+  function fetchAndMapUsers(members: string[]) {
+    console.log(members);
+    const usersRef = database.collection("users");
+    members.map(async (uid) => {
+      const snapshot = await usersRef.doc(uid).get();
+      const data = snapshot.data();
+      if (!data) return;
+      if ("imageUrl" in data) {
+        avatarUrlMap[uid] = data.imageUrl;
+      } else {
+        avatarUrlMap[uid] = "";
+      }
+    });
+
+    return avatarUrlMap;
+  }
+
+  const value = {
+    userAvatarMap,
+    fetchAndMapUsers,
+  };
+
+  return (
+    <AvatarContext.Provider value={value}>{children}</AvatarContext.Provider>
+  );
+}
