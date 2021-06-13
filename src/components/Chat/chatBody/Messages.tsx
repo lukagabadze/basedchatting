3e71b41d8c4efe9f85c3ff6839cf2da652938a0b
lukagabadze <gabadzeluka2003@gmail.com>
2021-06-13 23:35:41 +0400

    messages loading state + cleanup after every contact change

--- src/components/Chat/chatBody/Messages.tsx
+++ src/components/Chat/chatBody/Messages.tsx
@@ -1,3 +1,4 @@
+import { Typography } from "@material-ui/core";
 import { ReactElement, useState, useRef, useEffect } from "react";
 import { useAuth } from "../../../contexts/AuthContext";
 import { useAvatar } from "../../../contexts/AvatarContext";
@@ -11,20 +12,22 @@ interface Props {
 
 export default function Messages({ contact }: Props): ReactElement {
   const [messages, setMessages] = useState<MessageType[]>([]);
-  const messagesRef = useRef<MessageType[]>([]);
+  const [loading, setLoading] = useState(false);
   const chatDivRef = useRef<HTMLDivElement>(null);
 
   const urlMap = useAvatar(contact.members);
   const { user } = useAuth();
 
   useEffect(() => {
+    setLoading(true);
+    setMessages([]);
     const messagesRef = database.collection("messages");
-    messagesRef
+    const unsubscribe = messagesRef
       .where("contactId", "==", contact.id)
       .orderBy("createdAt", "desc")
       .limit(25)
       .onSnapshot((docSnap) => {
-        let messagesList: MessageType[] = [...messages];
+        let messagesList: MessageType[] = [];
         docSnap.forEach((doc) => {
           const { text, sender, contactId, createdAt } = doc.data();
           messagesList.push({
@@ -36,27 +39,33 @@ export default function Messages({ contact }: Props): ReactElement {
           });
         });
 
+        setLoading(false);
         setMessages(messagesList.reverse());
-
         // Scroll the user to the bottom
         if (chatDivRef.current) {
           chatDivRef.current.scrollTop = chatDivRef.current.scrollHeight;
         }
       });
+
+    return unsubscribe;
   }, [contact]);
 
   return (
     <div ref={chatDivRef} style={{ overflowY: "auto", height: "100%" }}>
-      {messages.map((message) => {
-        return (
-          <Message
-            key={message.id}
-            isOwn={message.sender === user?.uid}
-            message={message}
-            userImageUrl={urlMap[message.sender]}
-          />
-        );
-      })}
+      {!loading ? (
+        messages.map((message) => {
+          return (
+            <Message
+              key={message.id}
+              isOwn={message.sender === user?.uid}
+              message={message}
+              userImageUrl={urlMap[message.sender]}
+            />
+          );
+        })
+      ) : (
+        <Typography variant="h4">Loading...</Typography>
+      )}
     </div>
   );
 }
